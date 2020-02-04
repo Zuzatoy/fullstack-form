86c8785578891341150407da15089111d486e9f7
Lisa Serous <liza@realestate.co.nz>
2020-02-04 15:40:05 +1300

    prepare client for quering

--- client/src/client.js
+++ client/src/client.js
@@ -1,9 +1,22 @@
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloLink } from 'apollo-link'
 import { HttpLink } from 'apollo-link-http'
 import gql from 'graphql-tag'
 
 
-const client = new ApolloClient()
+const cache = new InMemoryCache()
+const http = new HttpLink({
+  uri: 'http://localhost:4000/'
+})
+
+const link = ApolloLink.from([
+  http
+])
+
+const client = new ApolloClient({
+  cache,
+  link
+})
 
 export default client
